b395ed28b0ca8c73cf1f8173bf556c0166acee47
duypt@brain-net.co.jp <duypt@brain-net.co.jp>
2024-03-01 09:06:33 +0700

    handle fb

--- public/web-tool/script.js
+++ public/web-tool/script.js
@@ -1,27 +1,18 @@
 flatpickr.localize(flatpickr.l10ns.vn);
 
-const firstDayOfMonth = new Date(new Date().getFullYear(), new Date().getMonth(), 1);
-const lastDayOfMonth = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0);
+const firstDayOfPreviousMonth = new Date(new Date().getFullYear(), new Date().getMonth() - 1, 1);
+const lastDayOfCurrentMonth = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0);
 
 const fromDateFp = flatpickr("#calendar-from-date", {
     altInput: true,
     altFormat: "d-m-Y",
     dateFormat: "Y-m-d",
     allowInput: false,
-    minDate: firstDayOfMonth,
-    maxDate: lastDayOfMonth,
+    minDate: firstDayOfPreviousMonth,
+    maxDate: lastDayOfCurrentMonth,
     disable: [
         function(date) {
             return (date.getDay() === 6 || date.getDay() === 0);
         }
     ]
 });
-
-// const toDateFp = flatpickr("#calendar-to-date", {
-//     altInput: true,
-//     altFormat: "d-m-Y",
-//     dateFormat: "Y-m-d",
-//     allowInput: false,
-//     minDate: firstDayOfMonth,
-//     maxDate: lastDayOfMonth,
-// });
