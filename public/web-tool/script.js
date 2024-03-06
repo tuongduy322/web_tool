a0cd767192881aca753bce4fb05cf24bdf1b5881
duypt@brain-net.co.jp <duypt@brain-net.co.jp>
2024-03-06 14:27:49 +0700

    handle fb

--- public/web-tool/script.js
+++ public/web-tool/script.js
@@ -1,15 +1,17 @@
 flatpickr.localize(flatpickr.l10ns.vn);
 
-const firstDayOfPreviousMonth = new Date(new Date().getFullYear(), new Date().getMonth() - 1, 1);
-const lastDayOfCurrentMonth = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0);
+const today = new Date();
+const quarterStartMonth = Math.floor(today.getMonth() / 3) * 3;
+const firstDayOfQuarter = new Date(today.getFullYear(), quarterStartMonth, 1);
+const lastDayOfQuarter = new Date(today.getFullYear(), quarterStartMonth + 3, 0);
 
 const fromDateFp = flatpickr("#calendar-from-date", {
     altInput: true,
     altFormat: "d-m-Y",
     dateFormat: "Y-m-d",
     allowInput: false,
-    minDate: firstDayOfPreviousMonth,
-    maxDate: lastDayOfCurrentMonth,
+    minDate: firstDayOfQuarter,
+    maxDate: lastDayOfQuarter,
     disable: [
         function(date) {
             return (date.getDay() === 6 || date.getDay() === 0);
